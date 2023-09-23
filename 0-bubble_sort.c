9131137c4166ca26220fda163ebbf50424a80152
bm-197 <bisratmaru111@gmail.com>
2023-09-23 05:01:53 -0700

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts a given array of integer using bubble sort algorithm
+ * array: the array to be sorted
+ * size: the size of the array
+ */
+
+void swap(*a, *b);
+
+void swap(*a, *b);
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void bubble_sort(int *array, size_t size)
+{
+	int flag;
+	int len = size;
+
+	for (i = 0; i < len; i++)
+	{
+		flag = 0;
+		for (j = 0; j < len - 1 - i; j++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(array[i], array[i + 1]);
+				print_array(array, size);
+				flag = 1;
+			}
+			if (flag = 0)
+				break;
+		}
+	}
+}
